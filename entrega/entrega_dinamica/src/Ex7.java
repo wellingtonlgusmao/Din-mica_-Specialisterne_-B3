9be80d920dae346f0c977959379661c858ad9abe
wellingtonlgusmao <wellingtonluizgusmao.29@gmail.com>
2022-10-20 11:32:14 -0300

    mod5

--- entrega/entrega_dinamica/src/Ex7.java
+++ entrega/entrega_dinamica/src/Ex7.java
@@ -19,15 +19,16 @@ public class Ex7 {
     public static void main(String[] args) {
 
         try (Scanner scan = new Scanner(System.in)) {
-            System.out.println("Digite um Número inteiro: ");
-            int numero = scan.nextInt();
-            int numeroqd = numero * numero;
-            int numerocubo = numeroqd * numero;
+            System.out.println("Digite um Número inteiro, diferente de zero: ");
+            // solicitando input da variavel,
+            // considernado exceção.
+            int numero = scan.nextInt();// recebendo
+            int numeroqd = numero * numero;// incrementando variaveis a partir do input
+            int numerocubo = numeroqd * numero;// incrementando variavel a partir de variavel
 
-            if (numero % 2 == 0)
+            if (numero % 2 == 0)// se o resto da divisão por 2 é igual a zero, logo
                 System.out.println("é um número par e seu quadrado é" + numeroqd);
-
-            else
+            else// caso contrário
                 System.out.println("é um número impar e seu cubo é" + numerocubo);
         }
 
